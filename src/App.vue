f571705fce337b85ac06ad7f7a6b337f614109a6
paulo martins <pgsm5@hotmail.com>
2020-11-28 18:39:02 -0300

    adicionado e visualizado componente POkemon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Pokemon from "./components/Pokemon";
 export default {
   name: 'App',
   data(){
@@ -18,6 +21,9 @@ export default {
       console.log("Peguei a lista");
       this.pokemons = res.data.results;
     });
+  },
+  components:{
+    Pokemon
   }
 }
 </script>
