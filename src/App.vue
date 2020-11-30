a5a7185c919ea980e54a2f8443f7ad2ce7d81680
Paulo <pgsm5@hotmail.com>
2020-11-30 11:28:29 -0300

    Busca de pokemon com botao

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
       <br>
       <h4 class="is-size-4">Pokedex</h4>
       <input type="text" name="" id="" placeholder="Buscar pokemon pelo nome" v-model="busca" class="input is-rounded">
-      <button id="btn-busca" class="button is-rounded">Pesquisar</button>
+      <button id="btn-busca" class="button is-rounded" @click="buscar">Pesquisar</button>
 
-      <div v-for="(poke,index) in resultadoBusca" :key="index">
+      <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
         <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
       </div>
     </div>
@@ -22,26 +22,37 @@ export default {
   data(){
     return{
       pokemons:[],
+      filteredPokemons:[],
       busca: ''
     }
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
-      console.log("Peguei a lista");
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
     });
   },
   components:{
     Pokemon
   },
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ''){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca.toLocaleLowerCase());
+      }
+    }
+  },
   computed:{
-    resultadoBusca: function(){
+    /*(resultadoBusca: function(){
       if(this.busca == '' || this.busca == ''){
         return this.pokemons;
       }else{
         return this.pokemons.filter(pokemon => pokemon.name == this.busca.toLocaleLowerCase());
       }
-    }
+    }*/
   }
 }
 </script>
